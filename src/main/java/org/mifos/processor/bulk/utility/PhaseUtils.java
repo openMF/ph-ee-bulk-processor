6a4265e21b69d5d0f526be9f5c0b2d522f43eb85
Ankita Ranjan <ankita@fynarfin.io>
2023-02-28 12:09:47 +0530

    Minor bug fix for variable name

--- src/main/java/org/mifos/processor/bulk/utility/PhaseUtils.java
+++ src/main/java/org/mifos/processor/bulk/utility/PhaseUtils.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
-@ConfigurationProperties(prefix = "callback_phases")
+@ConfigurationProperties(prefix = "callback-phases")
 public class PhaseUtils {
 
     private List<Integer> values;
