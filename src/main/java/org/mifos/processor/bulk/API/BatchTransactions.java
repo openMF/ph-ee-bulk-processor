bb13c16aa12ae1f8a14e9e41db3ffb251ff86fa3
Dhruv Sonagara <78945411+dhruvsonagara@users.noreply.github.com>
2023-04-04 16:29:45 +0530

    Migrated bulk transfer and batch transaction API (#52)
    * Migrated bulk transfer and batch transaction API
    
    * Reverted unnescessary changes
    
    * removed csv files

---
+++ src/main/java/org/mifos/processor/bulk/API/BatchTransactions.java
@@ -0,0 +1,25 @@
+package org.mifos.processor.bulk.API;
+
+import com.amazonaws.services.dynamodbv2.xspec.S;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.apache.camel.util.json.JsonObject;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.mail.Multipart;
+
+import java.io.IOException;
+
+import static org.mifos.processor.bulk.zeebe.ZeebeVariables.*;
+
+public interface BatchTransactions {
+
+    @PostMapping(value = "/batchtransactions", produces="application/json")
+    String batchTransactions(@RequestHeader(value = "X-CorrelationID") String requestId,
+                        @RequestParam("data") MultipartFile file,
+                        @RequestHeader(value = FILE_NAME) String fileName,
+                        @RequestHeader(value = PURPOSE) String purpose,
+                        @RequestHeader(value = "Type") String type,
+                        @RequestHeader(value = "Platform-TenantId") String tenant) throws IOException;
+}
