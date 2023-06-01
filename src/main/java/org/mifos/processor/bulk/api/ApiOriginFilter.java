fbb886a7ce7ee30fcaeffdc5c553f3d750cfce2b
Ishan Ranasingh <44061541+truthfool@users.noreply.github.com>
2023-06-01 12:44:49 +0530

    Added filter for logging tenant (#57)
    * removed optional
    
    * removed check

--- src/main/java/org/mifos/processor/bulk/api/ApiOriginFilter.java
+++ src/main/java/org/mifos/processor/bulk/api/ApiOriginFilter.java
@@ -27,11 +27,7 @@ public class ApiOriginFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
         String tenant = req.getHeader("Platform-TenantId");
-        Optional<String> tenantVal = Optional.of(tenant);
-        if (tenantVal.isPresent()) {
-            logger.info("Tenant Name is : {}", tenant);
-        }
-
+        logger.debug("Tenant Name is : {}", tenant);
     }
 
     @Override
