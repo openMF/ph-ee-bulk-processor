b894895c9a0ea28e8682d64fc6a5512c09583011
Ishan Ranasingh <44061541+truthfool@users.noreply.github.com>
2023-06-14 18:37:00 +0530

    [GOV-261] Logging client IP (#71)
    * logging client ip
    
    * changed log level

--- src/main/java/org/mifos/processor/bulk/api/ApiOriginFilter.java
+++ src/main/java/org/mifos/processor/bulk/api/ApiOriginFilter.java
@@ -28,6 +28,7 @@ public class ApiOriginFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         String tenant = req.getHeader("Platform-TenantId");
         logger.debug("Tenant Name is : {}", tenant);
+        logger.info("Client IP Address: {}",req.getRemoteHost());
     }
 
     @Override
