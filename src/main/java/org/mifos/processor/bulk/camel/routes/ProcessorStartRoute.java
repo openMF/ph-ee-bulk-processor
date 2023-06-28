64160e2a317aeab14fba52bea8bf04380bac6cd3
Ankita Ranjan <ankita@fynarfin.io>
2023-06-28 16:05:40 +0530

    adding extra log statements

--- src/main/java/org/mifos/processor/bulk/camel/routes/ProcessorStartRoute.java
+++ src/main/java/org/mifos/processor/bulk/camel/routes/ProcessorStartRoute.java
@@ -194,7 +194,9 @@ public class ProcessorStartRoute extends BaseRouteBuilder {
                     JSONObject response = new JSONObject();
 
                     try {
+                        logger.debug("Tenant name {}", exchange.getProperty(TENANT_NAME).toString());
                         String tenantSpecificWorkflowId = workflowId.replace("{dfspid}", exchange.getProperty(TENANT_NAME).toString());
+                        logger.debug("Starting workflow with id {}", tenantSpecificWorkflowId);
                         String txnId = zeebeProcessStarter.startZeebeWorkflow(tenantSpecificWorkflowId, "", variables);
                         if (txnId == null || txnId.isEmpty()) {
                             response.put("errorCode", 500);
