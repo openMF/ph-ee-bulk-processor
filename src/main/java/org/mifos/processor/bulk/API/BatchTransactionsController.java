52d10eba39ce58bab94c705dc297cfd0b764c0e3
DanishJamal <danishjamal.104@gmail.com>
2023-08-17 21:53:18 +0530

    Added multipart excecption handling

--- src/main/java/org/mifos/processor/bulk/API/BatchTransactionsController.java
+++ src/main/java/org/mifos/processor/bulk/API/BatchTransactionsController.java
@@ -6,11 +6,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.Exchange;
 import org.apache.camel.ProducerTemplate;
 import org.apache.tika.Tika;
+import org.json.JSONObject;
 import org.mifos.processor.bulk.file.FileStorageService;
 import org.mifos.processor.bulk.utility.Headers;
 import org.mifos.processor.bulk.utility.SpringWrapperUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.multipart.MultipartFile;
 import javax.activation.DataHandler;
 import javax.mail.MessagingException;
@@ -38,6 +41,7 @@ public class BatchTransactionsController implements BatchTransactions {
                                     String requestId, MultipartFile file, String fileName,
                                     String purpose, String type, String tenant) throws IOException {
         assert (file.getSize() >0);
+        log.info("Inside api logic");
         String localFileName = fileStorageService.save(file);
         Headers headers = new Headers.HeaderBuilder()
                 .addHeader("X-CorrelationID", requestId)
@@ -54,4 +58,19 @@ public class BatchTransactionsController implements BatchTransactions {
 		httpServletResponse.setStatus(statusCode);
        	return exchange.getIn().getBody(String.class);
     }
+
+    @ExceptionHandler({MultipartException.class})
+    public String handleMultipartException(HttpServletResponse httpServletResponse) {
+        JSONObject json = new JSONObject();
+        json.put("Error Information: ", "File not uploaded");
+        json.put("Error Description : ", "There was no fie uploaded with the request. " +
+                "Please upload a file and try again.");
+
+        httpServletResponse.setStatus(httpServletResponse.SC_BAD_REQUEST);
+//        httpServletResponse.getWriter().write(json.toString());
+//        httpServletResponse.getWriter().flush();
+
+        return json.toString();
+    }
+
 }
