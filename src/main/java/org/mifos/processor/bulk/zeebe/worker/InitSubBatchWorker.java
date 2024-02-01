91c82426474072e6cec3f37db94dd9ba197da6c8
abhinav <abhinav@Abhinavs-MacBook-Pro.local>
2024-02-01 11:35:25 +0530

    BatchId in transfers table logic updtae
    Removed unnecssary cvs
    
    enabled kafka port

--- src/main/java/org/mifos/processor/bulk/zeebe/worker/InitSubBatchWorker.java
+++ src/main/java/org/mifos/processor/bulk/zeebe/worker/InitSubBatchWorker.java
@@ -94,7 +94,6 @@ public class InitSubBatchWorker extends BaseWorker {
 
             Boolean subBatchFailed = exchange.getProperty(INIT_SUB_BATCH_FAILED, Boolean.class);
             if (subBatchFailed != null && subBatchFailed) {
-                failureSubBatches.add(fileName);
             } else {
                 successSubBatches.add(fileName);
             }
